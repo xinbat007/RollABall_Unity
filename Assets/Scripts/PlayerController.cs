cc7cd05203e55ab2174218f70207794dc466e365
Pham Quang Thien <thienpq88@gmail.com>
2017-04-08 00:21:00 +0700

    2nd finish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
+	public UnityEngine.UI.Text countText;
+	public UnityEngine.UI.Text winText;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		SetCountText();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -22,5 +27,24 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical");
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce(movement * speed);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12)
+		{
+			winText.text = "You Win";
+		}
 	}
 }
